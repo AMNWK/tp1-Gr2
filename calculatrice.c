e6f96d35fd552a0a4cb2a68c4306d7a94ca79d00
AMNWK <charonneuh@gmail.com>
2025-06-14 13:05:59 +0000

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -20,3 +20,15 @@ float soustraction(float a, float b) {
 
 
 //methode main par aida
+main() {
+    float a, b;
+    printf("Entrez deux nombres: ");
+    scanf("%f %f", &a, &b);
+    
+    printf("Addition: %.2f\n", addition(a, b));
+    printf("Soustraction: %.2f\n", soustraction(a, b));
+    printf("Multiplication: %.2f\n", multiplication(a, b));
+    printf("Division: %.2f\n", division(a, b));
+    
+    return 0;
+}
