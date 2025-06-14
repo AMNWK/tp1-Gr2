d52319039967ac08200338e3c790ee20b35965ae
AMNWK <charonneuh@gmail.com>
2025-06-14 12:16:14 +0000

    Update calculatrice.c

---
+++ calculatrice.c
@@ -0,0 +1,17 @@
+//directive de compilation par ange
+
+
+
+//fonction addition par amina
+
+
+//fonction soustraction par aida
+
+
+//fonction mutiplication par ange
+
+
+//fonction division par amina
+
+
+//methode main par aida
