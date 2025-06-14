6076644ee07d969b69698bc602f02f633847e863
aminata1709 <rassulaminatademe@gmail.com>
2025-06-14 12:56:02 +0000

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -3,7 +3,10 @@
 
 
 //fonction addition par amina
-
+float addition(float a,float b){
+    float s=a+b;
+    return s;
+}
 
 //fonction soustraction par aida
 
@@ -12,6 +15,10 @@
 
 
 //fonction division par amina
+float division(float a,float b){
+    float q=a/b;
+return q;
+}
 
 
 //methode main par aida
