de153b853093ea97d03eeec9f3ed788d685c4be1
AMNWK <charonneuh@gmail.com>
2025-06-14 12:59:45 +0000

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -1,31 +1,22 @@
 //directive de compilation par ange
-#include<stdio.h>
-#include<string.h>
-
-
 
 
 
 //fonction addition par amina
-float addition(float a,float b){
-    float s=a+b;
-    return s;
-}
+
 
 //fonction soustraction par aida
+float soustraction(float a, float b) {
+    return a - b;
+}
+
+
 
 
 //fonction mutiplication par ange
-float multiplication(float a, float b){
-    return a*b;
-}
 
 
 //fonction division par amina
-float division(float a,float b){
-    float q=a/b;
-return q;
-}
 
 
 //methode main par aida
